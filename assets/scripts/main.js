38f50f18904ebd59e24c2ee44db6b06e53f40da6
bsnow1400 <63069372+bsnow1400@users.noreply.github.com>
2022-11-07 20:18:15 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -75,7 +75,7 @@ function initFormHandler() {
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
   form.querySelector("button[type=submit]").addEventListener("click", (evt) => {
-    evt.preventDefault(); // Prevent page reload
+    event.preventDefault(); // Prevent page reload
 
     // Steps B4-B9 will occur inside the event listener from step B3
     // B4. TODO - Create a new FormData object from the <form> element reference above
